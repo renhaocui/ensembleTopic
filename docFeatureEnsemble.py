25616c086a7328f9f65c63b0dcf860e188ab3acd
renhaocui <renhao.cui88@gmail.com>
2016-03-15 23:35:08 -0400

    add prob feature only

--- docFeatureEnsemble.py
+++ docFeatureEnsemble.py
@@ -7,7 +7,7 @@ import sys
 from sklearn.linear_model import LogisticRegression
 from sklearn import svm
 
-def docAppend(vectorData, probData, labelCorpus, modelList, trainSize):
+def docAppend(vectorData, probData, labelCorpus, modelList, trainSize, useDocVector):
     probFeatures = []
     for index in range(trainSize):
         temp = []
@@ -18,10 +18,12 @@ def docAppend(vectorData, probData, labelCorpus, modelList, trainSize):
                 else:
                     temp.append(0.0)
         probFeatures.append(numpy.array(temp))
-
-    features = hstack((vectorData, csr_matrix(numpy.array(probFeatures))), format='csr')
-
+    if useDocVector:
+        features = hstack((vectorData, csr_matrix(numpy.array(probFeatures))), format='csr')
+    else:
+        features = csr_matrix(numpy.array(probFeatures))
     return features
+
 
 def testLabeler(features, testProbData, trueTestLabels, modelList, testSize):
     validIndex = []
@@ -71,14 +73,16 @@ def evaluator2(predictions, modelList, testProbData, testLabels):
         total += 1.0
     return correct/total
 
-def singleWeight(brandList, modelList):
+def singleWeight(brandList, modelList, useDocVector):
     print str(modelList)
-    resultFile = open('HybridData/Experiment/docFeature.result', 'a')
+    resultFile = open('HybridData/Experiment/probFeature.result', 'a')
     resultFile.write(str(modelList) + '\n')
     for brand in brandList:
         print brand
         accuracySum1 = 0.0
         accuracySum2 = 0.0
+        accuracySum3 = 0.0
+        accuracySum4 = 0.0
         trainIndex = {}
         testIndex = {}
         trainIndexFile = open('HybridData/Experiment/TrainIndex/'+brand+'.train', 'r')
@@ -100,13 +104,18 @@ def singleWeight(brandList, modelList):
 
             # read in
             docContent, labels, candTopic = modelUtility.readData('HybridData/Original/' + brand + '.content', 'HybridData/Original/' + brand + '.keyword')
-            vectorizer = TfidfVectorizer(analyzer='word', ngram_range=(1, 1), min_df=2, stop_words='english')
-            docVectors = vectorizer.fit_transform(docContent)
-            trainDocVector =docVectors[trainIndex[fold]]
-            testDocVector = docVectors[testIndex[fold]]
+            if useDocVector:
+                vectorizer = TfidfVectorizer(analyzer='word', ngram_range=(1, 1), min_df=2, stop_words='english')
+                docVectors = vectorizer.fit_transform(docContent)
+
+                trainDocVector =docVectors[trainIndex[fold]]
+                testDocVector = docVectors[testIndex[fold]]
+            else:
+                trainDocVector =[]
+                testDocVector = []
 
             # training
-            features = docAppend(trainDocVector, trainProbData, labelCorpus, modelList, trainSize)
+            features = docAppend(trainDocVector, trainProbData, labelCorpus, modelList, trainSize, useDocVector)
             trainLabels, validList = trainLabeler(trainProbData, trainLabels, modelList, trainSize)
             trainFeature = features[validList]
 
@@ -121,23 +130,29 @@ def singleWeight(brandList, modelList):
                 print 'Test data size error across models!'
                 sys.exit()
 
-            features = docAppend(testDocVector, testProbData, labelCorpus, modelList, testSize)
-            #testFeatures, labels = testLabeler(features, testProbData, testLabels, modelList, testSize)
-            testFeatures = features
+            features = docAppend(testDocVector, testProbData, labelCorpus, modelList, testSize, useDocVector)
+            testFeatures1, labels1 = testLabeler(features, testProbData, testLabels, modelList, testSize)
+            testFeatures2 = features
 
-            predictions1 = model1.predict(testFeatures)
-            predictions2 = model2.predict(testFeatures)
+            predictions1 = model1.predict(testFeatures1)
+            predictions2 = model2.predict(testFeatures1)
+            predictions3 = model1.predict(testFeatures2)
+            predictions4 = model2.predict(testFeatures2)
 
-            #accuracySum1 += evaluator(predictions1, labels)
-            accuracySum1 += evaluator2(predictions1, modelList, testProbData, testLabels)
-            accuracySum2 += evaluator2(predictions2, modelList, testProbData, testLabels)
-            #accuracySum2 += evaluator(predictions2, labels)
+            accuracySum1 += evaluator(predictions1, labels1)
+            accuracySum2 += evaluator(predictions2, labels1)
+            accuracySum3 += evaluator2(predictions3, modelList, testProbData, testLabels)
+            accuracySum4 += evaluator2(predictions4, modelList, testProbData, testLabels)
 
-        print 'MaxEnt\t '+str(accuracySum1 / 5.0)
-        print 'SMV\t'+str(accuracySum2 / 5.0)
+        print 'MaxEnt model selection\t '+str(accuracySum1 / 5.0)
+        print 'SMV model selection\t'+str(accuracySum2 / 5.0)
+        print 'MaxEnt label prediction\t'+str(accuracySum3 / 5.0)
+        print 'SMV label prediction\t'+str(accuracySum4 / 5.0)
 
         resultFile.write(brand + '\t' + str(accuracySum1 / 5.0) + '\n')
         resultFile.write(brand + '\t' + str(accuracySum2 / 5.0) + '\n')
+        resultFile.write(brand + '\t' + str(accuracySum3 / 5.0) + '\n')
+        resultFile.write(brand + '\t' + str(accuracySum4 / 5.0) + '\n')
 
     resultFile.close()
 
@@ -149,4 +164,4 @@ runModelList = [['MaxEnt', 'NaiveBayes'], ['LLDA', 'Alchemy'], ['LLDA', 'MaxEnt'
 
 if __name__ == "__main__":
     for modelList in runModelList:
-        singleWeight(brandList=brandList, modelList=modelList)
+        singleWeight(brandList=brandList, modelList=modelList, useDocVector=False)
